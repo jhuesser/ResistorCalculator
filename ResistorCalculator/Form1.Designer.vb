8a65ddd29e09a380dfbc8b641cad538a1e4789b1
jhuesser <jonas.huesser+github@gmail.com>
2016-01-22 21:57:18 +0100

    Calculate the rings

---
+++ ResistorCalculator/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbRing1 = New System.Windows.Forms.ComboBox()
+        Me.lblring1 = New System.Windows.Forms.Label()
+        Me.lblring2 = New System.Windows.Forms.Label()
+        Me.cmbRing2 = New System.Windows.Forms.ComboBox()
+        Me.lblRing3 = New System.Windows.Forms.Label()
+        Me.cmbRing3 = New System.Windows.Forms.ComboBox()
+        Me.lblRing4 = New System.Windows.Forms.Label()
+        Me.cmbRing4 = New System.Windows.Forms.ComboBox()
+        Me.chkRing5 = New System.Windows.Forms.CheckBox()
+        Me.lblRing5 = New System.Windows.Forms.Label()
+        Me.cmbRing5 = New System.Windows.Forms.ComboBox()
+        Me.btnCalcClr = New System.Windows.Forms.Button()
+        Me.lblResultString = New System.Windows.Forms.Label()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTol = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmbRing1
+        '
+        Me.cmbRing1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmbRing1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbRing1.FormattingEnabled = True
+        Me.cmbRing1.Location = New System.Drawing.Point(54, 34)
+        Me.cmbRing1.Name = "cmbRing1"
+        Me.cmbRing1.Size = New System.Drawing.Size(81, 21)
+        Me.cmbRing1.TabIndex = 0
+        '
+        'lblring1
+        '
+        Me.lblring1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblring1.AutoSize = True
+        Me.lblring1.Location = New System.Drawing.Point(12, 42)
+        Me.lblring1.Name = "lblring1"
+        Me.lblring1.Size = New System.Drawing.Size(36, 13)
+        Me.lblring1.TabIndex = 1
+        Me.lblring1.Text = "1. ring"
+        '
+        'lblring2
+        '
+        Me.lblring2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblring2.AutoSize = True
+        Me.lblring2.Location = New System.Drawing.Point(12, 81)
+        Me.lblring2.MinimumSize = New System.Drawing.Size(36, 13)
+        Me.lblring2.Name = "lblring2"
+        Me.lblring2.Size = New System.Drawing.Size(36, 13)
+        Me.lblring2.TabIndex = 2
+        Me.lblring2.Text = "2. ring"
+        '
+        'cmbRing2
+        '
+        Me.cmbRing2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmbRing2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbRing2.FormattingEnabled = True
+        Me.cmbRing2.Location = New System.Drawing.Point(54, 73)
+        Me.cmbRing2.Name = "cmbRing2"
+        Me.cmbRing2.Size = New System.Drawing.Size(81, 21)
+        Me.cmbRing2.TabIndex = 3
+        '
+        'lblRing3
+        '
+        Me.lblRing3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblRing3.AutoSize = True
+        Me.lblRing3.Location = New System.Drawing.Point(12, 117)
+        Me.lblRing3.Name = "lblRing3"
+        Me.lblRing3.Size = New System.Drawing.Size(36, 13)
+        Me.lblRing3.TabIndex = 4
+        Me.lblRing3.Text = "3. ring"
+        '
+        'cmbRing3
+        '
+        Me.cmbRing3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmbRing3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbRing3.FormattingEnabled = True
+        Me.cmbRing3.Location = New System.Drawing.Point(54, 109)
+        Me.cmbRing3.Name = "cmbRing3"
+        Me.cmbRing3.Size = New System.Drawing.Size(81, 21)
+        Me.cmbRing3.TabIndex = 5
+        '
+        'lblRing4
+        '
+        Me.lblRing4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblRing4.AutoSize = True
+        Me.lblRing4.Location = New System.Drawing.Point(164, 42)
+        Me.lblRing4.Name = "lblRing4"
+        Me.lblRing4.Size = New System.Drawing.Size(36, 13)
+        Me.lblRing4.TabIndex = 6
+        Me.lblRing4.Text = "4. ring"
+        '
+        'cmbRing4
+        '
+        Me.cmbRing4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmbRing4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbRing4.FormattingEnabled = True
+        Me.cmbRing4.Location = New System.Drawing.Point(206, 34)
+        Me.cmbRing4.Name = "cmbRing4"
+        Me.cmbRing4.Size = New System.Drawing.Size(81, 21)
+        Me.cmbRing4.TabIndex = 7
+        '
+        'chkRing5
+        '
+        Me.chkRing5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.chkRing5.AutoSize = True
+        Me.chkRing5.Location = New System.Drawing.Point(167, 86)
+        Me.chkRing5.Name = "chkRing5"
+        Me.chkRing5.Size = New System.Drawing.Size(57, 17)
+        Me.chkRing5.TabIndex = 8
+        Me.chkRing5.Text = "5 rings"
+        Me.chkRing5.UseVisualStyleBackColor = True
+        '
+        'lblRing5
+        '
+        Me.lblRing5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblRing5.AutoSize = True
+        Me.lblRing5.Location = New System.Drawing.Point(164, 117)
+        Me.lblRing5.Name = "lblRing5"
+        Me.lblRing5.Size = New System.Drawing.Size(36, 13)
+        Me.lblRing5.TabIndex = 9
+        Me.lblRing5.Text = "5. ring"
+        '
+        'cmbRing5
+        '
+        Me.cmbRing5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmbRing5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbRing5.FormattingEnabled = True
+        Me.cmbRing5.Location = New System.Drawing.Point(206, 109)
+        Me.cmbRing5.Name = "cmbRing5"
+        Me.cmbRing5.Size = New System.Drawing.Size(81, 21)
+        Me.cmbRing5.TabIndex = 10
+        '
+        'btnCalcClr
+        '
+        Me.btnCalcClr.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCalcClr.Location = New System.Drawing.Point(212, 236)
+        Me.btnCalcClr.Name = "btnCalcClr"
+        Me.btnCalcClr.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcClr.TabIndex = 11
+        Me.btnCalcClr.Text = "Calculate"
+        Me.btnCalcClr.UseVisualStyleBackColor = True
+        '
+        'lblResultString
+        '
+        Me.lblResultString.AutoSize = True
+        Me.lblResultString.Location = New System.Drawing.Point(13, 162)
+        Me.lblResultString.Name = "lblResultString"
+        Me.lblResultString.Size = New System.Drawing.Size(85, 13)
+        Me.lblResultString.TabIndex = 12
+        Me.lblResultString.Text = "Your resistor has"
+        '
+        'txtResult
+        '
+        Me.txtResult.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtResult.Location = New System.Drawing.Point(123, 162)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ReadOnly = True
+        Me.txtResult.Size = New System.Drawing.Size(164, 20)
+        Me.txtResult.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 197)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "with a tolerance of"
+        '
+        'txtTol
+        '
+        Me.txtTol.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtTol.Location = New System.Drawing.Point(123, 190)
+        Me.txtTol.Name = "txtTol"
+        Me.txtTol.ReadOnly = True
+        Me.txtTol.Size = New System.Drawing.Size(164, 20)
+        Me.txtTol.TabIndex = 15
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(305, 274)
+        Me.Controls.Add(Me.txtTol)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.lblResultString)
+        Me.Controls.Add(Me.btnCalcClr)
+        Me.Controls.Add(Me.cmbRing5)
+        Me.Controls.Add(Me.lblRing5)
+        Me.Controls.Add(Me.chkRing5)
+        Me.Controls.Add(Me.cmbRing4)
+        Me.Controls.Add(Me.lblRing4)
+        Me.Controls.Add(Me.cmbRing3)
+        Me.Controls.Add(Me.lblRing3)
+        Me.Controls.Add(Me.cmbRing2)
+        Me.Controls.Add(Me.lblring2)
+        Me.Controls.Add(Me.lblring1)
+        Me.Controls.Add(Me.cmbRing1)
+        Me.MinimumSize = New System.Drawing.Size(321, 274)
+        Me.Name = "frmMain"
+        Me.Text = "Resistor Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbRing1 As ComboBox
+    Friend WithEvents lblring1 As Label
+    Friend WithEvents lblring2 As Label
+    Friend WithEvents cmbRing2 As ComboBox
+    Friend WithEvents lblRing3 As Label
+    Friend WithEvents cmbRing3 As ComboBox
+    Friend WithEvents lblRing4 As Label
+    Friend WithEvents cmbRing4 As ComboBox
+    Friend WithEvents chkRing5 As CheckBox
+    Friend WithEvents lblRing5 As Label
+    Friend WithEvents cmbRing5 As ComboBox
+    Friend WithEvents btnCalcClr As Button
+    Friend WithEvents lblResultString As Label
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTol As TextBox
+End Class
---
+++ ResistorCalculator/Form1.vb
@@ -0,0 +1,432 @@
+﻿Public Class frmMain
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        'Colors of first ring
+        Dim color1(8) As String
+        color1 = {"brown", "red", "orange", "yellow", "green", "blue", "purple", "grey", "white"}
+
+        'Colors of second and third ring
+        Generate23()
+
+
+        'Colors of fourth ring
+        Generate4()
+
+
+        'Colors of fifth ring
+        Dim color5(2) As String
+        color5 = {"brown", "red", "green", "gold", "silver", "white"}
+
+
+
+
+
+        'Add colors for the first ring to the first dropdown
+        For Each color As String In color1
+            cmbRing1.Items.Add(color)
+
+
+        Next
+
+
+
+
+
+
+
+
+
+        'Add colors for the fith ring to the fourth dropdown
+        For Each color As String In color5
+            cmbRing5.Items.Add(color)
+
+
+        Next
+
+
+        EnableDisable5()
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub chkRing5_CheckedChanged(sender As Object, e As EventArgs) Handles chkRing5.CheckedChanged
+
+
+        EnableDisable5()
+        Generate4()
+        Generate23()
+
+
+
+
+    End Sub
+
+    Public Function EnableDisable5()
+
+
+
+        If chkRing5.Checked Then
+            lblRing5.Enabled = True
+            cmbRing5.Enabled = True
+
+
+
+
+        Else
+            lblRing5.Enabled = False
+            cmbRing5.Enabled = False
+
+
+
+        End If
+
+    End Function
+
+    Private Sub btnCalcClr_Click(sender As Object, e As EventArgs) Handles btnCalcClr.Click
+        valcolors()
+
+    End Sub
+
+
+
+    Private Function Generate4()
+        Dim FourRing As Boolean
+
+        FourRing = checkStatus()
+
+        If FourRing = True Then
+            cmbRing4.Items.Clear()
+            Dim color4(11) As String
+            color4 = {"brown", "red", "green", "blue", "purple", "gold", "silver", "white"}
+            For Each color In color4
+                cmbRing4.Items.Add(color)
+            Next
+        Else
+            cmbRing4.Items.Clear()
+            Dim color4(11) As String
+            color4 = {"silver", "gold", "black", "red", "orange", "yellow", "green", "blue", "purple", "grey", "white"}
+            For Each Color In color4
+                cmbRing4.Items.Add(Color)
+            Next
+
+        End If
+        Return FourRing
+
+    End Function
+
+
+    Private Function Generate23()
+        Dim FourRing As Boolean
+
+        FourRing = checkStatus()
+
+        If FourRing = True Then
+            cmbRing2.Items.Clear()
+            cmbRing3.Items.Clear()
+            Dim color2(9) As String
+            Dim color3(10) As String
+            color2 = {"black", "red", "orange", "yellow", "green", "blue", "purple", "grey", "white"}
+            color3 = {"black", "red", "orange", "yellow", "green", "blue", "purple", "grey"}
+            For Each color In color2
+                cmbRing2.Items.Add(color)
+            Next
+            For Each color In color3
+                cmbRing3.Items.Add(color)
+            Next
+        Else
+            cmbRing2.Items.Clear()
+            cmbRing3.Items.Clear()
+            Dim color23(9) As String
+            color23 = {"black", "red", "orange", "yellow", "green", "blue", "purple", "grey", "white"}
+            For Each Color In color23
+                cmbRing2.Items.Add(Color)
+                cmbRing3.Items.Add(Color)
+            Next
+
+        End If
+        Return FourRing
+
+    End Function
+
+    Private Function checkStatus()
+        Dim FourRing As Boolean
+
+        If chkRing5.Checked = True Then
+            FourRing = False
+        Else
+            FourRing = True
+
+        End If
+        Return FourRing
+
+    End Function
+
+    Public Sub valcolors()
+
+        Dim EndResult As Double
+        Dim fourRings As Boolean
+
+        fourRings = checkStatus()
+        ''TODO
+
+        If fourRings = False Then
+            Dim val1 As Single
+            Dim val2 As Single
+            Dim val3 As Single
+            Dim val4 As Single
+            Dim val5 As String
+
+            val1 = valFirst(val1)
+            val2 = valSecond(val2)
+            val3 = valThird(val3)
+            val4 = valFourth(val4)
+            val5 = valFifth(val5)
+            EndResult = calculate5(val1, val2, val3, val4)
+
+            txtResult.Text = EndResult & "Ω"
+            txtTol.Text = val5
+
+        ElseIf fourRings = True Then
+            Dim val1 As Single
+            Dim val2 As Single
+            Dim val3 As Single
+            Dim val4 As String
+
+
+            val1 = valFirst(val1)
+            val2 = valSecond(val2)
+            val3 = valThird2(val3)
+            val4 = valFourth2(val4)
+
+            EndResult = calculate4(val1, val2, val3)
+            txtResult.Text = EndResult & "Ω"
+            txtTol.Text = val4
+
+
+        End If
+
+
+
+
+    End Sub
+
+
+    Private Function valFirst(val1 As Single)
+
+        Select Case cmbRing1.Text
+            Case "brown"
+                val1 = 1
+            Case "red"
+                val1 = 2
+            Case "orange"
+                val1 = 3
+            Case "yellow"
+                val1 = 4
+            Case "green"
+                val1 = 5
+            Case "blue"
+                val1 = 6
+            Case "purple"
+                val1 = 7
+            Case "grey"
+                val1 = 8
+            Case "white"
+                val1 = 9
+
+
+        End Select
+        Return val1
+
+    End Function
+
+    Private Function valSecond(val2 As Single)
+
+        Select Case cmbRing2.Text
+            Case "black"
+                val2 = 0
+            Case "brown"
+                val2 = 1
+            Case "red"
+                val2 = 2
+            Case "orange"
+                val2 = 3
+            Case "yellow"
+                val2 = 4
+            Case "green"
+                val2 = 5
+            Case "blue"
+                val2 = 6
+            Case "purple"
+                val2 = 8
+            Case "white"
+                val2 = 9
+        End Select
+        Return val2
+    End Function
+
+    Private Function valThird(val3 As Single)
+        Select Case cmbRing3.Text
+            Case "black"
+                val3 = 0
+            Case "brown"
+                val3 = 1
+            Case "red"
+                val3 = 2
+            Case "orange"
+                val3 = 3
+            Case "yellow"
+                val3 = 4
+            Case "green"
+                val3 = 5
+            Case "blue"
+                val3 = 6
+            Case "purple"
+                val3 = 8
+            Case "white"
+                val3 = 9
+        End Select
+        Return val3
+    End Function
+
+    Private Function valThird2(val4 As Single)
+        Select Case cmbRing3.Text
+            Case "silver"
+                val4 = 0.01
+            Case "gold"
+                val4 = 0.1
+            Case "black"
+                val4 = 1
+            Case "brown"
+                val4 = 10
+            Case "red"
+                val4 = 100
+            Case "orange"
+                val4 = 1000
+            Case "yellow"
+                val4 = 10000
+            Case "green"
+                val4 = 100000
+            Case "blue"
+                val4 = 1000000
+            Case "purple"
+                val4 = 10000000
+            Case "grey"
+                val4 = 100000000
+
+
+        End Select
+        Return val4
+    End Function
+
+    Private Function valFourth(val4 As Single)
+        Select Case cmbRing4.Text
+            Case "silver"
+                val4 = 0.01
+            Case "gold"
+                val4 = 0.1
+            Case "black"
+                val4 = 1
+            Case "brown"
+                val4 = 10
+            Case "red"
+                val4 = 100
+            Case "orange"
+                val4 = 1000
+            Case "yellow"
+                val4 = 10000
+            Case "green"
+                val4 = 100000
+            Case "blue"
+                val4 = 1000000
+            Case "purple"
+                val4 = 10000000
+            Case "grey"
+                val4 = 100000000
+            Case "white"
+                val4 = 1000000000
+
+        End Select
+        Return val4
+    End Function
+
+    Private Function valFifth(val5 As String)
+        Select Case cmbRing5.Text
+            Case "brown"
+                val5 = "1%"
+            Case "red"
+                val5 = "2%"
+            Case "green"
+                val5 = "0.5%"
+            Case "blue"
+                val5 = "0.25%"
+            Case "purple"
+                val5 = "0.1%"
+            Case "gold"
+                val5 = "5%"
+            Case "silver"
+                val5 = "10%"
+            Case "white"
+                val5 = "20%"
+
+        End Select
+
+        Return val5
+    End Function
+
+    Private Function valFourth2(val5 As String)
+        Select Case cmbRing4.Text
+            Case "brown"
+                val5 = "1%"
+            Case "red"
+                val5 = "2%"
+            Case "green"
+                val5 = "0.5%"
+            Case "blue"
+                val5 = "0.25%"
+            Case "purple"
+                val5 = "0.1%"
+            Case "gold"
+                val5 = "5%"
+            Case "silver"
+                val5 = "10%"
+            Case "white"
+                val5 = "20%"
+
+        End Select
+
+        Return val5
+    End Function
+
+    Private Function calculate5(val1 As Single, val2 As Single, val3 As Single, val4 As Single)
+
+        Dim EndResult5 As Double
+
+
+        EndResult5 = val1 & val2 & val3
+        EndResult5 = EndResult5 * val4
+
+
+
+        Return EndResult5
+
+
+    End Function
+
+    Private Function calculate4(val1 As Single, val2 As Single, val3 As Single)
+
+        Dim EndResult As Single
+        EndResult = val1 & val2
+        EndResult = EndResult * val3
+        Return EndResult
+
+    End Function
+
+
+End Class
